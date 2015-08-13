6dc881dad626fd2ed2ee8a1bbc6f1de891db2843
zl810881283 <810881283@qq.com>
2015-08-13 12:10:39 +0800

    new

---
+++ cal/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using test_add;
+using MathWorks.MATLAB.NET.Arrays;
+
+namespace cal
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cal_Click(object sender, EventArgs e)
+        {
+            MatlabAdd matlabAdd = new MatlabAdd();
+            MWArray a = double.Parse(inputA.Text), b = double.Parse(inputB.Text);
+            MWNumericArray c = (MWNumericArray)matlabAdd.test_add(a, b);
+            Array arr=c.ToArray();
+
+            result.Text = c.ToScalarDouble() + "";
+
+            MWNumericArray x = new MWNumericArray(8,2,new double[16]{
+                1,11,
+                2,12,
+                3,13,
+                4,14,
+                5,15,
+                6,16,
+                7,17,
+                8,18
+            });
+            matlabAdd.test_plot(x);
+        }
+    }
+}
