064a6e30b9fb464ea50397008af8f7f0154349e7
zkq <1191482087@qq.com>
2016-01-24 13:08:03 +0800

    增加图例高度缩放

--- GroundWellDesign/Converter.cs
+++ GroundWellDesign/Converter.cs
@@ -34,16 +34,26 @@ namespace GroundWellDesign
                 case "yanXing":
                     string s = value as string;
                     return Application.Current.FindResource(s);
-                case "windowH":
-                    double h = (double)value;
-                    return h * 3;
-
             }
             return null;
-
         }
 
         public object ConvertBack(object value, Type targetType, object param, CultureInfo c)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    public class ItemHeightConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            return values.Length == 2 ? (double)values[0] * (double)values[1] : 0;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
